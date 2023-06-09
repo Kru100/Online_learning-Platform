d927a107e21227335f35dbf6d8622deea9e14ba5
Hiten324812 <hitenmistry354@gmail.com>
2023-06-09 11:56:12 +0530

    first integration

--- authenticating/urls.py
+++ authenticating/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from authenticating import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('signup/', views.registerAPI, name='signup'),
     path('otp/',views.otp, name='otp'),
     path('login/',views.loginAPI, name='login'),
     path('forget-password/',views.forgetpasswordAPI, name='forget-password'),
-    path('change_password/<token>/',views.changpasswordAPI, name='change_password')
-]
+    path('change_password/<token>/',views.changpasswordAPI, name='change_password'),
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
