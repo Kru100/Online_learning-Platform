6c3af6b9347b3420b5f386b8c02c8920e8eacf60
Hiten324812 <hitenmistry354@gmail.com>
2023-06-15 20:50:57 +0530

    forget password 2

--- instructor/urls.py
+++ instructor/urls.py
@@ -4,5 +4,6 @@ from instructor import views
 urlpatterns = [
     path('add-course/', views.add_course, name='add-course'),
     path('enrolledCourse/',views.enrolledCourse, name='enrollment'),
-    path('display/',views.showenrolled, name='display')
+    path('display/',views.showenrolled, name='display'),
+    path('instructor_list/',views.ins_list,name='ins_list')
 ]
