6e83a98c54653407f04e49ebb24c6276a434fe6d
Krunal <@kp1584085@gmail.com>
2023-06-22 12:14:40 +0530

    Some work on frontend and also some backend bugs.

--- instructor/urls.py
+++ instructor/urls.py
@@ -4,17 +4,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('home/',views.home, name='home'),
     path('add-course/', views.add_course, name='add-course'),
     path('enrolledCourse/',views.enrolledCourse, name='enrollment'),
-    path('display/',views.showenrolled, name='display'),
     path('display2/',views.courselist, name='display2'),
-    path('course/<int:course_id>/',views.createQuiz, name='course'),
+    path('quiz/<int:course_id>/',views.createQuiz, name='quizz'),
     path('quiz/<int:course_id>/<int:quiz_id>/', views.quizdisplay, name='quiz'),
     path('quizshow/<int:quiz_id>/', views.quiz_show, name='quiz_show'),
     path('video/<int:course_id>/',views.video_upload, name='video_upload'),
     path('quiz/update1/<int:course_id>/<int:quiz_id>/<int:id>/',views.edit_question,name='edit_question'),
     path('quiz/update2/<int:course_id>/<int:quiz_id>/<int:id>/',views.update_question,name='update_question'),
     path('quiz/delete/<int:course_id>/<int:quiz_id>/<int:id>/',views.delete_question,name='delete_question'),
+    path('course/<int:course_id>/',views.courseHome, name='course'),
+    path('course/delete/<int:course_id>', views.delete_course, name='delete_course')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
