4d0a13734a0ee80a94a318fdba17de658e962da3
Hiten324812 <hitenmistry354@gmail.com>
2023-06-23 23:09:25 +0530

    logout functionality

--- student/views.py
+++ student/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from instructor.models import User
+from django.contrib.auth.decorators import login_required
+from authenticating.views import *
+
 
 # Create your views here.
+
+@custom_login_required
+def indexpage(request):
+    email = request.session.get('email')
+    user = User.objects.get(email=email)
+    return render(request,'home.html',{'user': user})
+
