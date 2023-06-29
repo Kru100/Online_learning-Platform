b7114cc8ac6ef8a8c143cfe86510eee5381dfc36
Hiten324812 <hitenmistry354@gmail.com>
2023-06-29 18:09:49 +0530

    some changes

--- authenticating/urls.py
+++ authenticating/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home2'),
     path('signup/', views.registerAPI, name='signup'),
     path('otp/',views.otp, name='otp'),
     path('accounts/login/',views.loginAPI, name='login'),
